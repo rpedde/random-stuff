19d96ad6319667b1fced98b6ba8e0100b1daa00c
rpedde <ron@pedde.com>
2013-06-26 22:00:56 -0500

    add mif format and power-of-two padding to dumpfont

--- dumpfont.py
+++ dumpfont.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 
+import getopt
 import gzip
 import sys
 import os
 import struct
-
-filename=sys.argv[1]
-
-print 'opening %s' % filename
 
 class PSFFile(object):
     def __init__(self, filename):
@@ -16,7 +13,7 @@ class PSFFile(object):
         self.load_font()
 
     def load_font(self):
-        fh = gzip.GzipFile(filename, 'r')
+        fh = gzip.GzipFile(self.filename, 'r')
 
         header_format = "cccc"
 
@@ -59,15 +56,85 @@ class PSFFile(object):
         print 'Mode: %02x (%s)' % (
             self.filemode, modes[self.filemode])
 
-    def dump_file(self, filename):
+        pad_size = 1
+        while(pad_size < self.fontheight):
+            pad_size *= 2
+
+        self.padding = pad_size - self.fontheight
+        print 'Padding: %d' % self.padding
+
+
+    def dump_file(self, filename, padding=False, mif=False):
         f = open(filename, 'w')
         for x in range(0, 256):
-            f.write(''.join([chr(i) for i in self.bitmap_data[x]]))
+            if mif:
+                for data in self.bitmap_data[x]:
+                    f.write(to_bin(data) + '\n')
+            else:
+                f.write(''.join([chr(i) for i in self.bitmap_data[x]]))
+
+            if padding:
+                for _ in range(0, self.padding):
+                    if mif:
+                        f.write('00000000\n')
+                    else:
+                        f.write(chr(0))
         f.close()
 
+def to_bin(i):
+    str_return = ''
+    while i:
+        if i & 1:
+            str_return = '1' + str_return
+        else:
+            str_return = '0' + str_return
+        i = i >> 1
+
+    return ('00000000' + str_return)[-8:]
+
+def usage_quit():
+    print "Usage: dumpfont <options>\n"
+    print "Options:"
+    print " -i <infile>    input filename"
+    print " -o <outfile>   output filename"
+    print " -p             pad to even power of 2"
+    print " -m             output in mif format instead of binary"
+    sys.exit(1)
 
 
-font = PSFFile(filename)
-font.info()
-font.dump_char('A')
-font.dump_file('out.fon')
+def main():
+    in_file = None
+    out_file = None
+    pad = False
+    mif = False
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'i:o:pm')
+    except getopt.GetoptError, err:
+        print str(err)
+        usage_quit()
+
+
+    for o, a in opts:
+        if o == '-i':
+            in_file = a
+        elif o == '-o':
+            out_file = a
+        elif o == '-p':
+            pad = True
+        elif o == '-m':
+            mif = True
+        else:
+            print 'Unknown option: %s' % o
+            usage_quit()
+
+    if in_file is None or out_file is None:
+        print "Input and output file name is required\n"
+        usage_quit()
+
+    font = PSFFile(in_file)
+    font.info()
+    font.dump_char('A')
+    font.dump_file(out_file, pad, mif)
+
+main()
